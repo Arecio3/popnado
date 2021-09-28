f1d9b627366c13dc7b66fcd1b1bf0cc9245e27e5
Arecio Canton <cuba289@gmail.com>
2021-09-28 05:15:11 -0400

    added func for checking elevation height and assign it a color

--- map1.py
+++ map1.py
@@ -11,21 +11,32 @@ lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 volc_name = list(data['NAME'])
-
+# string of html for link and info
 html = """
+<div class="volc_info">
 <b>Volcano name:</b><br>
-<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
-Height: %s m
+<a href="https://www.google.com/search?q=%%22%s%%22" id="volc_link" target="_blank">%s</a><br>
+Height: %s m </div>
 """
 
 # tiles gives us terrain map
 map = folium.Map(location=[28.01381155418516, -82.56964496261388],tiles = "Stamen Terrain", zoom_start=5)
 
+# check elevation
+def elevation(elev):
+        if elev < 2000:
+            return 'green'
+        elif elev >= 2000 and elev < 3000:
+            return 'orange'
+        else:
+            return 'red'
+
 # zip merges lists
 for lt, ln, el, volc in zip(lat, lon, elev, volc_name):
+    # iframe is for adding html
     iframe = folium.IFrame(html=html % (volc, volc, el), width=200, height=100)
     # add markers to map
-    fg.add_child(folium.Marker(location=[lt,ln] , popup=folium.Popup(iframe), icon=folium.Icon(color='red', icon="glyphicon-fire")))
+    fg.add_child(folium.Marker(location=[lt,ln] , popup=folium.Popup(iframe), icon=folium.Icon(color=elevation(el), icon="glyphicon-fire")))
 
 map.add_child(fg)
 
