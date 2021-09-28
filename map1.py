18313b8d189ce3e12a457b7998baa1afc4d2c231
Arecio Canton <cuba289@gmail.com>
2021-09-28 03:54:34 -0400

    read data and grabbed volcano location and plotted all with a for loop

--- map1.py
+++ map1.py
@@ -1,12 +1,22 @@
 import folium
+import pandas
+# use pandas to read data file
+data = pandas.read_csv('Webmap_datasources/Volcanoes.txt')
+
+# grabs values from data column and converts them to lists
+lat = list(data['LAT'])
+lon = list(data['LON'])
+volc_name = list(data['NAME'])
 # tiles gives us terrain map
 map = folium.Map(location=[28.01381155418516, -82.56964496261388],tiles = "Stamen Terrain")
 
 # By making this you have orginzation and will help when you want to add layer control feature later
 fg = folium.FeatureGroup(name="My Map")
 
+# zip merges both lists
+for lt, ln in zip(lat, lon):
 # add markers to map
-fg.add_child(folium.Marker(location=[28.01381155418516, -82.56964496261388], popup='Hey there im a marker', icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln] , popup='hello there', icon=folium.Icon(color='red')))
 
 map.add_child(fg)
 
